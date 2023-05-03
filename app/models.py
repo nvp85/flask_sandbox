b15843967f389a3df7050939068f65dfe63aeafb
nvp85 <nvp@zhukov.al>
2023-05-03 00:03:31 -0700

    fix login. add logout

--- app/models.py
+++ app/models.py
@@ -31,5 +31,6 @@ class Post(db.Model):
     
 @login.user_loader
 def load_user(id):
+    
     return User.query.get(int(id))
 
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from app import app
 from flask import render_template, redirect, flash, url_for
 from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user
+from app.models import User
 
 
 @app.route('/')
@@ -22,17 +24,21 @@ def index():
 
 @app.route('/login', methods=['GET','POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
-        return redirect(url_for('index'))
-    return render_template('login.html', form=form)
-
-@app.route('/login', methods=['GET','POST'])
-def login():
-    if current_user.isauthenticated:
+    if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid ussername or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+        
         
