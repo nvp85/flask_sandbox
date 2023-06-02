0b06147b619931ccf99f2cf672e61f096e01c5cd
nvp85 <nvp@zhukov.al>
2023-06-01 23:56:23 -0700

    fix the index view

--- app/routes.py
+++ app/routes.py
@@ -10,19 +10,8 @@ from datetime import datetime
 @app.route('/')
 @app.route('/index')
 def index():
-    # let's temporarly mock a user object
-    user = {'username': 'test_user'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    
+    return render_template('index.html', title='Home', posts=posts)
 
 @app.route('/login', methods=['GET','POST'])
 def login():
