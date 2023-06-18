0605b7e9f5251707fc6210876769d96bb377bf37
nvp85 <nvp@zhukov.al>
2023-06-17 23:00:15 -0700

    add follow/unfollow methods to the user model

--- app/models.py
+++ app/models.py
@@ -32,6 +32,18 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    
+    def is_following(self, user):
+        return self.followed.filter(followers.c.followed_id == user.id).count() > 0
+    
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
