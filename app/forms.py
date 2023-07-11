aca838b49388144d59507d404df4a2802c26feab
nvp85 <nvp@zhukov.al>
2023-07-10 23:25:15 -0700

    add the post form

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
 from app.models import User
 
@@ -43,3 +43,9 @@ class EditProfileForm(FlaskForm):
         
 class EmptyForm(FlaskForm):
     submit = SubmitField('submit')
+
+
+class PostForm(FlaskForm):
+    post = TextAreaField('Say something', validators=[
+        DataRequired(), Length(min=1, max=140)])
+    submit = SubmitField('Submit')
